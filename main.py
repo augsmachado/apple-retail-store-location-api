d19a9972006d25390bba14fdcd4ac2a97ba2b0f3
Augs Machado <augustomachadoramos@gmail.com>
2023-09-11 23:42:48 -0300

    [main.py] feat: add status code when get store details is empty

--- main.py
+++ main.py
@@ -86,7 +86,12 @@ def get_stores_by_country(country: str):
 # database based on the provided `store_id` parameter.
 @app.get("/api/v1/stores/{store_id}")
 def get_store_details(store_id: str):
-    return db.get(store_id)
+    res = db.get(store_id)
+
+    if res is None:
+        raise HTTPException(status_code=404, detail="Store not found")
+
+    return res
 
 
 # `@app.post("/stores")` is a decorator in FastAPI that defines a route for handling POST requests to
